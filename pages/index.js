719f14261b56a52c59044b2494d0399e265d3c5d
tatsuya-shirai <midon.tatsuya@gmail.com>
2021-03-28 18:20:40 +0900

    ♻️ Headline部分をコンポーネント化

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Links } from "../components/Links";
+import Headline from "../components/Headline";
 
 export default function Home() {
   return (
@@ -13,12 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Index page</h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Headline page="index" />
 
         <Links />
       </main>
